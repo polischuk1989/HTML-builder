d492bfc0904684a0fdae2911cef02804a85d3ab1
dmitriy <89271436436@mail.ru>
2022-05-24 03:06:51 +0400

    add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,21 +7,37 @@ fs.mkdir(path.resolve(__dirname,'files-copy' ),  {recursive:true},err => {
   console.log('Папка успешно создана');
 });
 
+fs.readdir(path.resolve(__dirname,'files-copy' ), (err, files) => {
+  files.forEach(file => {
+    if (err) throw err;
+    fs.unlink(path.join(__dirname,`files-copy/${file}`), err => {
+      
+      if(err) throw err; // не удалось удалить файл
+      console.log('Файл успешно удалён');
+    });
+  });
+});
+
 fs.readdir(path.resolve(__dirname,'files' ),  (err, files) => {
   files.forEach(file => {
+    
+    
+    
     fs.copyFile(path.resolve(__dirname,`files/${file}`), path.resolve(__dirname,`files-copy/${file}` ), err => {
       if(err) throw err; // не удалось скопировать файл
       console.log('Файл успешно скопирован');
     });
-      
-     
-  
   });
   
 });
 
 
-/*fs.copyFile('old_file.txt', 'new_file.txt', err => {
-  if(err) throw err; // не удалось скопировать файл
-  console.log('Файл успешно скопирован');
-});*/
+
+
+
+
+/*fs.unlink(path.join(__dirname,`files-copy/${file}`), err => {
+      
+    if(err) throw err; // не удалось удалить файл
+    console.log('Файл успешно удалён');
+  });*/
